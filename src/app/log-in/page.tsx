9dac7c7f880365f0fac5314886f8b0c7810e95b7
z1zonemoi <zizonemoi@gmail.com>
2023-01-31 22:44:28 +0900

    💰 feat : 소셜로그인 기능구현, 로그인-회원가입 페이지 라우트처리

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -1,8 +1,14 @@
 "use client";
 import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "firebase/auth";
 import { auth } from "../../Firebase";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function LogIn() {
   const router = useRouter();
@@ -16,7 +22,7 @@ export default function LogIn() {
     setForm({ ...form, [name]: value });
   };
 
-  const onSubmitSignUp = async (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitLogIn = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
       let data = await signInWithEmailAndPassword(
@@ -33,44 +39,80 @@ export default function LogIn() {
     }
   };
 
+  const onClickSocialLogIn = async (
+    event: React.MouseEvent<HTMLButtonElement> | undefined,
+  ) => {
+    const { name } = event?.target;
+    console.log(name);
+
+    let provider;
+    if (name === "google") {
+      provider = new GoogleAuthProvider();
+    }
+    if (name === "github") {
+      provider = new GithubAuthProvider();
+    }
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log(result.operationType);
+      if (result.operationType === "signIn") {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="border-solid border-2 border-black h-screen flex flex-col items-center justify-center justify-items-center ">
-      <form
-        onSubmit={onSubmitSignUp}
-        className="w-[30rem] h-[35rem] overflow-scroll border-solid border-2 border-black rounded-3xl flex flex-col justify-items-center items-center"
-      >
-        <div className="w-96 h-24 text-6xl text-center font-[Galmuri9]">
-          Log In
-        </div>
-        <input
-          name="email"
-          value={form.email}
-          onChange={onChangeForm}
-          className="w-96 h-14"
-          type="text"
-          placeholder="이메일을 입력해주세요"
-        />
-        <input
-          name="password"
-          value={form.password}
-          onChange={onChangeForm}
-          className="w-96 h-14"
-          type="password"
-          placeholder="비밀번호를 입력해주세요"
-        />
-        <button type="submit" className="w-96 h-28">
-          Log In
-        </button>
-        <button type="button" className="w-96 h-12">
+      <div className="form-wrapper w-[30rem] h-[35rem] overflow-scroll border-solid border-2 border-black rounded-3xl flex flex-col justify-items-center items-center">
+        <form
+          onSubmit={onSubmitLogIn}
+          className="w-[30rem] h-[20rem] flex flex-col justify-items-center items-center"
+        >
+          <div className="w-96 h-24 text-6xl text-center font-[Galmuri9]">
+            Log In
+          </div>
+          <input
+            name="email"
+            value={form.email}
+            onChange={onChangeForm}
+            className="w-96 h-14"
+            type="text"
+            placeholder="이메일을 입력해주세요"
+          />
+          <input
+            name="password"
+            value={form.password}
+            onChange={onChangeForm}
+            className="w-96 h-14"
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+          />
+          <button type="submit" className="w-96 h-28">
+            Log In
+          </button>
+        </form>
+        <button
+          name="google"
+          onClick={onClickSocialLogIn}
+          type="button"
+          className="w-96 h-12"
+        >
           구글 로그인
         </button>
-        <button type="button" className="w-96 h-12">
+        <button
+          onClick={onClickSocialLogIn}
+          name="github"
+          type="button"
+          className="w-96 h-12"
+        >
           깃허브 로그인
         </button>
-        <button type="button" className="w-96 h-12">
+        <Link href="/sign-up" className="w-96 h-12 text-center">
           회원가입
-        </button>
-      </form>
+        </Link>
+      </div>
     </div>
   );
 }
