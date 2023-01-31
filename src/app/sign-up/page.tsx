30b646b85780287bfaef06da23b8855973cffe18
z1zonemoi <zizonemoi@gmail.com>
2023-01-31 14:04:52 +0900

    💰 feat : sign-up submit 새로고침 현상 방지

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -13,15 +13,21 @@ export default function SignUp() {
     setForm({ ...form, [name]: value });
   };
 
-  const onSubmitSignUp = (event: React.FormEvent<HTMLButtonElement>) =>
+  const onSubmitSignUp = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    console.log(form);
+  };
 
   return (
     <div className="border-solid border-2 border-black h-screen flex flex-col items-center justify-center justify-items-center ">
-      <form className="w-[30rem] h-[35rem] overflow-scroll border-solid border-2 border-black rounded-3xl flex flex-col justify-items-center items-center">
+      <form
+        onSubmit={onSubmitSignUp}
+        className="w-[30rem] h-[35rem] overflow-scroll border-solid border-2 border-black rounded-3xl flex flex-col justify-items-center items-center"
+      >
         <div className="w-96 h-24 text-6xl text-center ">sign up</div>
         <input
           name="email"
+          value={form.email}
           onChange={onChangeForm}
           className="w-96 h-14"
           type="text"
@@ -29,6 +35,7 @@ export default function SignUp() {
         />
         <input
           name="password"
+          value={form.password}
           onChange={onChangeForm}
           className="w-96 h-14"
           type="password"
@@ -36,12 +43,13 @@ export default function SignUp() {
         />
         <input
           name="verifyPassword"
+          value={form.verifyPassword}
           onChange={onChangeForm}
           className="w-96 h-14"
           type="password"
           placeholder="비밀번호를 다시 입력해주세요"
         />
-        <button onSubmit={onSubmitSignUp} className="w-96 h-28" type="submit">
+        <button type="submit" className="w-96 h-28">
           회원가입
         </button>
       </form>
