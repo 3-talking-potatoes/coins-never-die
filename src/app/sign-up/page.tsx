c19e5b408d45f748d117195199486928b7e3d557
z1zonemoi <zizonemoi@gmail.com>
2023-02-07 21:04:42 +0900

    #25 💎 style : 회원가입 페이지 tailwindcss적용

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -7,6 +7,7 @@ import { useRouter } from "next/navigation";
 
 import { auth, db } from "../../Firebase";
 import Link from "next/link";
+import { RiBitCoinFill } from "react-icons/ri";
 
 export default function SignUp() {
   const router = useRouter();
@@ -55,42 +56,52 @@ export default function SignUp() {
   };
 
   return (
-    <div className="border-solid border-2 border-black h-screen flex flex-col items-center justify-center justify-items-center ">
-      <div className="form-wrapper w-[30rem] h-[35rem] overflow-scroll border-solid border-2 border-black rounded-3xl flex flex-col justify-items-center items-center">
+    <div className="bg-yellow-100 h-screen flex flex-col items-center justify-center justify-items-center ">
+      <div className="bg-white form-wrapper w-[30rem] h-[35rem] overflow-x-hidden border-solid border-[3px] border-black rounded-3xl flex flex-col justify-items-center items-center">
         <form
           onSubmit={onSubmitSignUp}
-          className="w-[30rem] h-[20rem] flex flex-col justify-items-center items-center"
+          autoComplete="off"
+          className="w-[30rem] h-[26rem] flex flex-col justify-items-center items-center"
         >
-          <div className="w-96 h-24 text-6xl text-center ">sign up</div>
+          <RiBitCoinFill className="w-[4rem] h-[4rem] mt-8 text-yellow-200" />
+          <div className="w-80 h-12 mb-6 text-3xl text-blue text-center font-[Galmuri7] rounded-lg">
+            Sign up
+          </div>
           <input
             name="email"
             value={form.email}
             onChange={onChangeForm}
-            className="w-96 h-14"
+            className="w-80 h-10 p-3  placeholder:text-black-200 text-black text-lg mb-4 border-solid border-[3px] border-black rounded-lg focus:outline-yellow-200  focus:text-yellow-200"
             type="text"
-            placeholder="이메일을 입력해주세요"
+            placeholder="이메일을 입력해주세요."
           />
           <input
             name="password"
             value={form.password}
             onChange={onChangeForm}
-            className="w-96 h-14"
+            className="w-80 h-10 p-3 placeholder:text-black-200 text-lg mb-4 border-solid border-[3px] border-black rounded-lg  focus:outline-yellow-200 focus:text-yellow-200"
             type="password"
-            placeholder="비밀번호를 입력해주세요"
+            placeholder="비밀번호를 입력해주세요."
           />
           <input
             name="verifyPassword"
             value={form.verifyPassword}
             onChange={onChangeForm}
-            className="w-96 h-14"
+            className="w-80 h-10 p-3 placeholder:text-black-200 text-lg mb-4 border-solid border-[3px] border-black rounded-lg  focus:outline-yellow-200 focus:text-yellow-200"
             type="password"
-            placeholder="비밀번호를 다시 입력해주세요"
+            placeholder="비밀번호를 다시 입력해주세요."
           />
-          <button type="submit" className="w-96 h-28">
-            회원가입
+          <button
+            type="submit"
+            className="w-80 h-20 font-[Galmuri7] text-2xl mt-4 border-solid border-[3px] border-black bg-yellow-200 rounded-lg hover:bg-yellow-100 hover:text-3xl"
+          >
+            Sign Up
           </button>
         </form>
-        <Link href="/log-in" className="w-96 h-12 text-center">
+        <Link
+          href="/log-in"
+          className="w-fit h-8 mt-12 text-center hover:text-xl"
+        >
           로그인
         </Link>
       </div>
