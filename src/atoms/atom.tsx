ac11c1cd2a0d96df7328c3f001c0f1e3b1e479a1
ichenny <ichenny.lee@gmail.com>
2023-02-08 15:25:33 +0900

    #36 💵  fix: useeffect 무한루프로 인한 가격계산 오류 수정

--- src/atoms/atom.tsx
+++ src/atoms/atom.tsx
@@ -32,15 +32,15 @@ export const userId = atom({
 
 export const tradingOrderQuantity = atom({
   key: "tradingOrderQuantity",
-  default: 0,
+  default: "0",
 });
 
 export const tradingTotalOrderAmount = atom({
   key: "tradingTotalOrderAmount",
-  default: 0,
+  default: "0",
 });
 
-export const tradingPurchasePrice = atom<number | string>({
+export const tradingPurchasePrice = atom({
   key: "tradingPurchasePrice",
   default: "",
 });
@@ -48,4 +48,14 @@ export const tradingPurchasePrice = atom<number | string>({
 export const myAssetCash = atom({
   key: "myAssetCash",
   default: 100000,
+});
+
+export const tradingIsOrderQuantityChanged = atom({
+  key: "isOrderQuantity",
+  default: false,
+});
+
+export const tradingIsTotalOderAmountChanged = atom({
+  key: "isTotalOderAmount",
+  default: false,
 });
