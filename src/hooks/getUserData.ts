6810bfd5b873a2c80a72eb495b0454fb486193a8
z1zonemoi <zizonemoi@gmail.com>
2023-02-08 10:33:16 +0900

    25 💰 feat : getUserData 함수 구현, recoil 연결

--- src/hooks/getUserData.ts
+++ src/hooks/getUserData.ts
@@ -1,10 +1,15 @@
 import { db } from "./../Firebase";
 import { doc, onSnapshot } from "firebase/firestore";
+import { SetterOrUpdater } from "recoil";
 
-export const getUserData = (userUid: string) => {
+export const getUserData = (
+  userUid: string,
+  setUserAssetData: SetterOrUpdater<{}>,
+) => {
   if (userUid !== "") {
     onSnapshot(doc(db, "user", userUid), doc => {
-      console.log("Current data: ", doc.data());
+      const data = doc.data();
+      setUserAssetData({ ...data });
     });
   }
 };
