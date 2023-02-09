a307bb3669c7a6892dd37ca03d3465703eeb5ec3
94sssh <abizsh@naver.com>
2023-02-09 14:49:55 +0900

    #39 💷 refactor : 메인페이지 관련 컴포넌트 리팩토링

--- src/api/getCoinData.ts
+++ src/api/getCoinData.ts
@@ -1,17 +1,22 @@
-import { getCoinList } from "@/api/getCoinList";
 import axios from "axios";
 import { coinList, coinData } from "@/interface/interface";
 
 export const getCoinData = async () => {
-  let url = await getCoinList();
-
-  const map = new Map();
-  url.forEach((item: coinList) => map.set(item.market, item));
-
-  url = url.map((el: coinList) => el.market).join();
-
   const options = { method: "GET", headers: { accept: "application/json" } };
+
   try {
+    const responseName = await axios.get(
+      "https://api.upbit.com/v1/market/all?isDetails=false",
+      options,
+    );
+    let url = responseName.data.filter((el: coinList) =>
+      el.market.includes("KRW"),
+    );
+
+    const map = new Map();
+    url.forEach((item: coinList) => map.set(item.market, item));
+    url = url.map((el: coinList) => el.market).join();
+
     const response = await axios.get(
       `https://api.upbit.com/v1/ticker?markets=${url}`,
       options,
