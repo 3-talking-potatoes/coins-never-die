66af9a95bae79cdcd2b2b09bf136e1fe0deadc04
ichenny <ichenny.lee@gmail.com>
2023-02-10 16:06:48 +0900

    #67 💎 style: 보유자산 코인리스트 css 수정

--- src/components/MyAsset/MyAssetCoinList.tsx
+++ src/components/MyAsset/MyAssetCoinList.tsx
@@ -1,15 +1,21 @@
 "use client";
 
-import { useRecoilValue } from "recoil";
+import { useRecoilValue, useRecoilState } from "recoil";
 
-import { userUidAssetData } from "@/atoms/atom";
+import { userUidAssetData, myAssetIsCoinListClick } from "@/atoms/atom";
 import { IcurrentPrice } from "@/interface/interface";
 
 import { RiBitCoinFill } from "react-icons/ri";
 
 const MyAssetCoinList = ({ currentPrice }: { currentPrice: IcurrentPrice }) => {
   const userAssetData = useRecoilValue(userUidAssetData);
-  console.log(userAssetData.asset?.data);
+  const [isCoinListClick, setIsCoinListClick] = useRecoilState(
+    myAssetIsCoinListClick,
+  );
+
+  const handleCoinListClick = () => {
+    setIsCoinListClick(prev => !prev);
+  };
 
   return (
     <div>
@@ -28,7 +34,10 @@ const MyAssetCoinList = ({ currentPrice }: { currentPrice: IcurrentPrice }) => {
 
           return (
             <div key={name}>
-              <figure className="h-12 px-2.5 pt-1 border-b border-grey">
+              <figure
+                className="h-12 px-2.5 pt-1 border-b border-grey"
+                onClick={handleCoinListClick}
+              >
                 <div className="flex items-center">
                   <RiBitCoinFill className="text-4xl text-yellow-coin w-[10%]" />
                   <p className="w-[30%] flex justify-center">{name}</p>
@@ -40,32 +49,34 @@ const MyAssetCoinList = ({ currentPrice }: { currentPrice: IcurrentPrice }) => {
                   <p className="w-[30%] flex justify-center">{earningRate}%</p>
                 </div>
               </figure>
-              <figure>
-                <div className="flex py-1">
-                  <div className="w-1/2 flex flex-col items-end px-2.5">
-                    <p>{`${value.numberOfShares} ${name}`}</p>
-                    <p>보유수량</p>
+              {isCoinListClick && (
+                <figure>
+                  <div className="flex py-1">
+                    <div className="w-1/2 flex flex-col items-end px-2.5">
+                      <p>{`${value.numberOfShares} ${name}`}</p>
+                      <p>보유수량</p>
+                    </div>
+                    <div className="w-1/2 flex flex-col items-end px-2.5">
+                      <p>{`${averagePurchasePrice} KRW`}</p>
+                      <p>매수평균가</p>
+                    </div>
                   </div>
-                  <div className="w-1/2 flex flex-col items-end px-2.5">
-                    <p>{`${averagePurchasePrice} KRW`}</p>
-                    <p>매수평균가</p>
+                  <div className="flex py-1">
+                    <div className="w-1/2 flex flex-col items-end px-2.5">
+                      <p>{`${new Intl.NumberFormat("ko-KR").format(
+                        equitiesValue,
+                      )} KRW`}</p>
+                      <p>평가금액</p>
+                    </div>
+                    <div className="w-1/2 flex flex-col items-end px-2.5">
+                      <p>{`${new Intl.NumberFormat("ko-KR").format(
+                        value.buyAmount,
+                      )} KRW`}</p>
+                      <p>매수금액</p>
+                    </div>
                   </div>
-                </div>
-                <div className="flex py-1">
-                  <div className="w-1/2 flex flex-col items-end px-2.5">
-                    <p>{`${new Intl.NumberFormat("ko-KR").format(
-                      equitiesValue,
-                    )} KRW`}</p>
-                    <p>평가금액</p>
-                  </div>
-                  <div className="w-1/2 flex flex-col items-end px-2.5">
-                    <p>{`${new Intl.NumberFormat("ko-KR").format(
-                      value.buyAmount,
-                    )} KRW`}</p>
-                    <p>매수금액</p>
-                  </div>
-                </div>
-              </figure>
+                </figure>
+              )}
             </div>
           );
         })}
