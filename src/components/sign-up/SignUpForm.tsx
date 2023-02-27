fe506dc70233618943d07241b0c64928bab82b21
z1zonemoi <zizonemoi@gmail.com>
2023-02-27 22:58:32 +0900

    #144 💎 style : 로그인, 회원가입 페이지 반응형 아이폰 12 pro 기준

--- src/components/sign-up/SignUpForm.tsx
+++ src/components/sign-up/SignUpForm.tsx
@@ -24,21 +24,22 @@ export default function SignUpForm() {
       <Image
         src="/cndIcon.png"
         alt="icon"
-        width="100"
-        height="100"
-        className="ml-[16rem] mb-[-2rem] md:ml-[21rem] md:mb-[-2rem]"
+        width={100}
+        height={100}
+        priority
+        className="ml-[14rem] mb-[-2rem] md:ml-[21rem] md:mb-[-2rem]"
       />
-      <div className="bg-white form-wrapper w-[25rem] md:w-[30rem] h-[35rem] overflow-x-hidden scrollbar-hide border-solid border-[3px] border-black rounded-3xl flex flex-col justify-items-center items-center">
+      <div className="bg-white form-wrapper w-[22rem] md:w-[30rem] h-[35rem] overflow-x-hidden scrollbar-hide border-solid border-[3px] border-black rounded-3xl flex flex-col justify-items-center items-center">
         <div>
           <RiBitCoinFill className="w-[3rem] h-[3rem] mt-3 text-yellow-200 dark:text-purple-200" />
         </div>
-        <div className="w-80 h-10 md:h-12 mb-1 text-2xl md:text-3xl text-blue text-center font-[Galmuri7] rounded-lg">
+        <div className="w-[22rem] h-10 md:h-12 mb-1 text-2xl md:text-3xl text-blue text-center font-[Galmuri7] rounded-lg">
           Sign up
         </div>
         <form
           onSubmit={onSubmitSignUp}
           autoComplete="off"
-          className="w-[30rem] h-[20rem] flex flex-col justify-items-center items-center"
+          className="w-[22rem] h-[20rem] flex flex-col justify-items-center items-center"
         >
           <SignUpFormInput
             name="email"
