2e4b9f56a3b51908d28cc4cba184f3040422d8c1
94sssh <abizsh@naver.com>
2023-02-27 22:14:36 +0900

    #142 💎 style : 메인페이지 모바일 반응형 css 구현

--- src/components/main/Search.tsx
+++ src/components/main/Search.tsx
@@ -15,10 +15,10 @@ const Search = () => {
   } = useSearch();
 
   return (
-    <div className="mb-1 w-[30rem] sm:w-[34rem] md:w-[36rem] lg:w-[50rem] xl:w-[64rem] flex-none">
-      <div className="h-[44px] flex border-2 px-2 sm:px-2 lg:px-4 border-yellow-200 dark:border-purple-200 rounded-lg bg-white ">
+    <div className="mb-1 w-[22rem] sm:w-[34rem] md:w-[36rem] lg:w-[50rem] xl:w-[64rem] flex-none">
+      <div className="h-[44px] flex border-2 px-2 sm:px-6 md:px-8 lg:px-10 border-yellow-200 dark:border-purple-200 rounded-lg bg-white ">
         <input
-          className="focus:outline-none sm:ml-px md:ml-1 lg:ml-6 w-full"
+          className="focus:outline-none w-full"
           placeholder="찾고 싶은 코인을 입력하세요"
           onChange={handleInputChange}
           value={inputValue}
@@ -28,11 +28,11 @@ const Search = () => {
         ></input>
       </div>
       {hasText ? (
-        <ul className="z-10 absolute w-[30rem] sm:w-[34rem] md:w-[36rem] lg:w-[50rem] xl:w-[64rem] items-center justify-start border-2 border-yellow-200 dark:border-purple-200 rounded-lg bg-white">
+        <ul className="z-10 absolute w-[22rem] sm:w-[34rem] md:w-[36rem] lg:w-[50rem] xl:w-[64rem] items-center justify-start border-2 border-yellow-200 dark:border-purple-200 rounded-lg bg-white">
           {coinResult.map((el, index) => (
             <li
               id={index.toString()}
-              className={`flex list-none h-8 px-10 items-center ${
+              className={`flex list-none h-8 px-2 sm:px-6 md:px-8 lg:px-10 items-center ${
                 index === state.selectedIndex ? "bg-grey" : "bg-white"
               } `}
               role="presentation"
